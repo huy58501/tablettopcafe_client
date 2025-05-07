f22d5e63d5fda3c07abe9d1f87ce9dfe0978a770
HOANG CHUONG NGUYEN <97963939+huy58501@users.noreply.github.com>
2025-05-07 15:33:58 -0400

    Update Tables.tsx

--- src/components/client/tables/Tables.tsx
+++ src/components/client/tables/Tables.tsx
@@ -99,20 +99,6 @@ const Tables: React.FC = () => {
       setOrderLoading(true);
       const mappedTables = [...tablesData.allTable]
         .sort((a: Table, b: Table) => a.id - b.id)
-        .map((table: Table) => {
-          const bookingWithOrder = table.bookings?.find(booking => booking.order);
-          const isOccupied = !!bookingWithOrder;
-          return {
-            ...table,
-            status: isOccupied ? 'occupied' : table.status,
-            orders: bookingWithOrder?.order
-              ? ({
-                  ...bookingWithOrder.order,
-                  orderItems: bookingWithOrder.order.orderItems || [],
-                } as ExtendedOrder)
-              : null,
-          };
-        });
       console.log('mappedTable: ', mappedTables);
       setTables(mappedTables);
 
